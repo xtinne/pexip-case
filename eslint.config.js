2ddf6921c7881fce32dbfc80fc78b3e99a3dee43
Tinne Jacobs <jacobstinne1@gmail.com>
2026-08-24 15:52:28 +0200

    Initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js'
+import importX from 'eslint-plugin-import-x'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'import-x': importX,
+    },
+    rules: {
+      'import-x/order': [
+        'error',
+        {
+          'newlines-between': 'never',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'unknown',
+            'parent',
+            'sibling',
+            'index',
+            'type',
+          ],
+        },
+      ],
+    },
+  },
+])
